50930fff6ef25676e02de0462b6f8bbb380ec4ca
BIMA <bima46bayu25@gmail..com>
2023-08-29 20:45:06 +0700

    pertemuan 2

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get("/about",[Controller::class, "index"]);
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "lala",
+        "email" => "lala@gmail.com"
+    ]);
+});
+// Route::get("/about",[Controller::class, "index"]);
